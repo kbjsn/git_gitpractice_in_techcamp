681e06e54773c34e942a22a4e436b2cd6f6549cd
EC2 Default User <ec2-user@ip-172-31-39-231.ap-northeast-1.compute.internal>
2020-09-20 05:53:30 +0000

    トップページへアクセスした場合に動くアクションを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'tweets' => 'tweets#index'
-  get 'tweets/new' => 'tweets#new'
-  post 'tweets' => 'tweets#create'
+  root 'tweets#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
